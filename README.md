98554161bc1d10f92ab1a180d9d22428feb1e43b
Daniel <naivar@pm.me>
2025-01-18 15:10:17 -0700

    Added python script and README.md files.

--- README.md
+++ README.md
@@ -1,2 +1,71 @@
-# Multi-Systsem-Updater-Linux-
-This python script is meant to automate the system update process for users who want to keep up with the latest security patches available.
+
+# Nala Update Utility
+
+## Overview
+
+The Nala Update Utility is a Python script designed to help users manage system updates and create system snapshots. It provides a user-friendly interface to perform various update-related tasks using different package managers.
+
+## Features
+
+- **Create System Snapshots**: Allows users to create a snapshot of the current system state using Timeshift.
+- **Update System**: Provides options to update the system using various package managers such as `apt`, `nala`, `yum`, `dnf`, and `pacman`.
+- **System Reboot**: Offers an option to reboot the system after performing updates.
+
+## Usage
+
+### Running the Script
+
+To run the script, execute the following command in your terminal:
+
+```bash
+chmod +x update.py
+./update.py
+```
+or, if you prefer to use Python directly:`
+```
+
+```bash
+python3 update.py
+```
+
+(Example output below)
+
+Welcome to the Nala Update Utility!
+Please select from the following options:
+
+1. Update the system
+2. Exit
+
+Preparing to update the system,
+
+Please select from the following options:
+
+1. Create a snapshot of the current system
+2. Update the system
+3. System reboot
+
+
+Which package manager would you like to use?
+
+1. apt and apt-get
+2. nala package wrapper for Apt
+3. yum
+4. dnf
+5. pacman and yayRequirements
+6. exit
+
+
+Python 3.x
+Timeshift (for creating snapshots)
+Appropriate package managers installed (apt, nala, yum, dnf, pacman, yay)
+
+
+
+
+
+
+
+## License
+*This project is licensed under the MIT License.*
+
+
---
+++ update.py
@@ -0,0 +1,235 @@
+#! /usr/bin/python3
+
+# The multi-upgrade utility for Linux systems
+
+
+import subprocess as sp
+
+
+# Upadte Function
+
+def update():
+    sp.run(['clear'])
+    print("Preparing to update the system, \n")
+    print("Please select from the following options:")
+    print("""
+    1. Create a snapshot of the current system
+    2. Update the system
+    3. System reboot
+    """)
+
+    choice = input("Enter your choice: ")
+
+    if choice == '1':
+        print("Enter a name for the snapshot, or leave empty for none: ")
+        name = input()
+
+        try:
+            print("Creating snapshot with Timeshift...")
+            sp.run(['sudo', 'timeshift', '--create', '--comment', name])
+            print(f"Snapshot {name} created successfully!")
+            print("Here is a list of your snapshots for confirmation:\n")
+            sp.run(['sudo', 'timeshift', '--list'])
+            print("\n")
+            print("Press any key to continue...")
+            input()
+            main()
+        except Exception as e:
+            print(f"An error occured: {e}")
+            print("Press any key to continue...")
+            input()
+            main()
+
+    elif choice == '2':
+        sp.run(['clear'])
+        print("Which package manager would you like to use?\n")
+        print("""
+        1. apt and apt-get
+        2 .nala package wrapper for Apt
+        3 yum
+        4 dnf
+        5. pacman and yay
+        6 exit
+        """)
+
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            print("Updating the system with apt and apt-get...")
+            try:
+                sp.run(['sudo', 'apt', 'update', '-y'])
+                sp.run(['sudo', 'apt', 'upgrade', '-y'])
+                print("System updated successfully!")
+                print("do you need to upgrade the distribution? (y/n)")
+                dist = input()
+                distro = dist.lower()
+
+                if distro == 'y':
+                    print("Upgrading the distribution...")
+
+                    try:
+                        sp.run(['sudo', 'apt', 'dist-upgrade', '-y'])
+                        print("Distribution upgraded successfully!")
+                        print("Press any key to continue...")
+                        input()
+                        main()
+
+                    except Exception as e:
+                        print(f"An error occured: {e}")
+                        print("Press any key to continue...")
+                        input()
+                        main()
+                else:
+                    print("Distribution upgrade skipped.")
+                    print("Press any key to continue...")
+                    input()
+                    main()
+
+            except Exception as e:
+                print(f"An error occured: {e}")
+                print("Press any key to continue...")
+                input()
+                main()
+
+        elif choice == '2':
+            print("Updating the system with nala package wrapper for Apt...")
+            try:
+                sp.run(['sudo', 'nala', 'update', '-y'])
+                sp.run(['sudo', 'nala', 'upgrade', '-y'])
+                print("System updated successfully!")
+                print("do you need to upgrade the distribution? (y/n)")
+                dist = input()
+                distro = dist.lower()
+
+                if distro == 'y':
+                    print("Upgrading the distribution...")
+
+                    try:
+                        sp.run(['sudo', 'nala', 'dist-upgrade', '-y'])
+                        print("Distribution upgraded successfully!")
+                        print("Press any key to continue...")
+                        input()
+                        main()
+
+                    except Exception as e:
+                        print(f"An error occured: {e}")
+                        print("Press any key to continue...")
+                        input()
+                        main()
+                else:
+                    print("Distribution upgrade skipped.")
+                    print("Press any key to continue...")
+                    input()
+                    main()
+
+            except Exception as e:
+                print(f"An error occured: {e}")
+                print("Press any key to continue...")
+                input()
+                main()
+
+        elif choice == '3':
+            print("Updating the system with yum...")
+            try:
+                sp.run(['sudo', 'yum', 'update', '-y'])
+                print("System updated successfully!")
+                print("Press any key to continue...")
+                input()
+                main()
+
+            except Exception as e:
+                print(f"An error occured: {e}")
+                print("Press any key to continue...")
+                input()
+                main()
+
+        elif choice == '4':
+            print("Updating the system with dnf...")
+            try:
+                sp.run(['sudo', 'dnf', 'update', '-y'])
+                print("System updated successfully!")
+                print("Press any key to continue...")
+                input()
+                main()
+
+            except Exception as e:
+                print(f"An error occured: {e}")
+                print("Press any key to continue...")
+                input()
+                main()
+
+        elif choice == '5':
+            print("Updating the system with pacman and yay...")
+            try:
+                sp.run(['sudo', 'pacman', '-Syu', '--noconfirm'])
+                print("System updated successfully!")
+                print("Press any key to continue...")
+                input()
+                main()
+
+            except Exception as e:
+                print(f"An error occured: {e}")
+                print("Press any key to continue...")
+                input()
+                main()
+
+        elif choice == '6':
+            print("Exiting the utility...")
+            raise SystemExit
+        else:
+            print("Invalid choice, please try again.")
+            print("Press any key to continue...")
+            input()
+            main()
+
+    elif choice == '3':
+        print("Rebooting the system...")
+        try:
+            sp.run(['sudo', 'reboot'])
+        except Exception as e:
+            print(f"An error occured: {e}")
+            print("Press any key to continue...")
+            input()
+            main()
+
+    else:
+        print("Invalid choice, please try again.")
+        print("Press any key to continue...")
+        input()
+        main()
+
+
+
+
+
+# Main Function
+
+def main():
+    sp.run(['clear'])
+    print("Welcome to the Nala Update Utility!")
+    print("Please select from the following options:")
+    print("""
+    1. Update the system
+    2. Exit
+    """)
+
+    choice = input("Enter your choice: ")
+
+    if choice == '1':
+        update()
+    elif choice == '2':
+        print("Exiting the utility...")
+        exit()
+    else:
+        print("Invalid choice, please try again.")
+        print("Press any key to continue...")
+        input()
+        main()
+
+# Main Function Call
+
+if __name__ == '__main__':
+    main()
+
+
+
